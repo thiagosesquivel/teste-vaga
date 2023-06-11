dfab5d4e2a8cf74d1a7fb2a5059739324644dc9a
Thiago Esquivel <thiago.sesquivel@gmail.com>
2023-06-11 14:41:12 -0300

    implement get access method

--- teste5.js
+++ teste5.js
@@ -1,9 +1,15 @@
+const data = require('./fakeData');
 
+module.exports = function (req, res) {
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+    const name = req.header.username;
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+    const user = data?.find((d) => d.name?.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") == name);
+
+    if (!user) {
+        res.status(404).send("Usuário não encontrado");
+    }
+
+    res.send(`Usuário ${user?.name} foi lido ${user?.counter ?? 0} vezes.`);
 
 };

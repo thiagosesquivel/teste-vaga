3916155886517b1db9d9002599ed5c2694515081
Thiago Esquivel <thiago.sesquivel@gmail.com>
2023-06-11 14:40:01 -0300

    refactor method get one

--- teste1.js
+++ teste1.js
@@ -1,21 +1,34 @@
-var data =  require("./fakeData");
+let data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res) => {
+    // Adicionei uma normalização, deixando tudo minúscula e sem caracteres especiais, 
+    //desta forma a pesquisa não é case sensitive e não levará em consideração os caracteres especiais
+    const name = req.header.username;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
+    /*
+    A busca está para trazer um único usuário, mas poderíamos trazer uma lista
+     de usuários cujo o nome contém a string que está sendo passada, com as linhas 
+     de código abaixo:*/
+
+    // const user = data.filter((d) => d.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(name));
+
+    // if (!user.length) {
+    //     res.status(404).send("Usuário não encontrado");
+    // }
+
+    const user = data.find((d) => d.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") == name);
+
+    if (!user) {
+        res.status(404).send("Usuário não encontrado");
     }
+
+    user.counter = user.counter ? user.counter + 1 : 1;
+    res.send(user);
 
 };
 
-const getUsers = ( req, res, next ) => {
-    
+const getUsers = (req, res) => {
     res.send(data);
-    
 };
 
 module.exports = {
